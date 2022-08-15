89adce522490f1ee89c62eeecadc1be642e2f0a2
Rednata <maknatosh@gmail.com>
2022-08-15 16:00:19 +0700

    ready!

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,28 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        let arr = [];
+        this.arr= arr;
+        arr[0] = min;
+        arr[1] = max;
     }
 
-    guess() {
-
+    guess() {        
+        let newNumber;
+        newNumber = Math.round((this.arr[0] + this.arr[1])/2);
+        this.newNumber=newNumber;
+        console.log(newNumber);
+        return newNumber;
     }
 
     lower() {
-
+        this.arr[1] = this.newNumber;
     }
 
     greater() {
-
+        this.arr[0] = this.newNumber;
     }
 }
 
